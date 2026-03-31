49a7a638cb31d88e683d8e4d5a02d786dff1a9fb
trangqlds <tl.datascience@gmail.com>
2026-04-01 00:07:39 +0100

    cicd

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'youtube',
+    version= '0.0.0',
+    author= 'tql',
+    author_email= 'tl@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+
+)
